2faaa8e8ce136cd444a7e5e31aeb570d34d7c79b
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-10-15 20:30:04 +0530

    created a html file

--- day-2.js
+++ day-2.js
@@ -1,11 +1,19 @@
 // Leec 2 : if-else
 
 // 11] can vote or not
-let age = Number(prompt("enter agee"));
-if (age >= 18) {
-    console.log('can vote');
+// let age = Number(prompt("enter agee"));
+// if (age>=18) {
+//     console.log('can vote');
     
-} else {
-    console.log('cant vote');
+// } else {
+//     console.log('cant vote');
     
+// }
+
+// 12] DIScount shop 
+let amount = Number(prompt("enter amout"));
+if (amount >= 0 && amount <=5000){
+    console.log( amount,'no discount');
+}else if(amount>=5000 && amount<=7000) {
+    console.log(amount - (Math.floor(5*amount)/100))
 }
