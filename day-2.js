b86606cd561a4353d79c200d39a9525a72ea2557
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-10-15 19:42:27 +0530

    started

---
+++ day-2.js
@@ -0,0 +1,11 @@
+// Leec 2 : if-else
+
+// 11] can vote or not
+let age = Number(prompt("enter agee"));
+if (age >= 18) {
+    console.log('can vote');
+    
+} else {
+    console.log('cant vote');
+    
+}
