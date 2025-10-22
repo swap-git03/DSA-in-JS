d2404068811c79c68425c8002d46eb1229ea249c
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-10-22 19:54:59 +0530

    day 2 done on 22-10-2025

--- day-2.js
+++ day-2.js
@@ -51,3 +51,32 @@
 // }
 // amount += units* 4//400
 // console.log(amount);
+
+
+// 14] INR Distributioon
+// let amount = 4888;
+// if(amount >500){
+//     console.log('500 rs notes will be :' + Math.floor(amount/500));
+//     amount = amount%500;
+// }
+// if(amount >200){
+//     console.log('200 rs notes will be :' + Math.floor(amount/200));
+//     amount = amount%200;
+// }
+// if(amount >100){
+//     console.log('100 rs notes will be :' + Math.floor(amount/100));
+//     amount = amount%100;
+
+// }if(amount >50){
+//     console.log('50 rs notes will be :' + Math.floor(amount/50));
+//     amount = amount%50;
+// }if(amount >20){
+//     console.log('20 rs notes will be :' + Math.floor(amount/20));
+//     amount = amount%20;
+// }if(amount >10){
+//     console.log('10 rs notes will be :' + Math.floor(amount/10));
+//     amount = amount%10;
+// }
+// if(amount >9){
+//     console.log('1 rs doller will be :' + amount);
+// }
