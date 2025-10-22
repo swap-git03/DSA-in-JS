d2d1e0cb7d61060cdb203df0338c8d1c8cd7ed84
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-10-22 16:18:59 +0530

    /

--- day-2.js
+++ day-2.js
@@ -11,19 +11,43 @@
 // }
 
 // 12] DIScount shop 
-let amount = Number(prompt("enter amout"));
-let discount = 0;
-if (amount >= 0 && amount <=5000){
-    console.log( amount,'no discount');
-}else if(amount>=5001 && amount<=7000) {
-    discount = 5;
-}else if(amount>=7001 && amount<=9000) {
-    discount = 10;
-}
-else if(amount>=9000) {
-    discount = 20;
-}
-else{
-    console.log('noot a amount');
-}
-console.log(amount - Math.floor((amount*discount)/100));
+// let amount = Number(prompt("enter amout"));
+// let discount = 0;
+// if (amount >= 0 && amount <=5000){
+//     console.log( amount,'no discount');
+// }else if(amount>=5001 && amount<=7000) {
+//     discount = 5;
+// }else if(amount>=7001 && amount<=9000) {
+//     discount = 10;
+// }
+// else if(amount>=9000) {
+//     discount = 20;
+// }
+// else{
+//     console.log('noot a amount');
+// }
+// console.log(amount - Math.floor((amount*discount)/100));
+
+
+// 13] Bijli Bill
+// let units = Number(prompt('Enter units'))
+// console.log('units are',units);
+// let amount = 0;
+
+
+// if (units > 400) {
+//     amount = (units - 400) * 13; //3900
+//     units = 400;
+// }
+// if (units > 200 && units <=400 ) {
+//     amount += (units - 200) * 8; //1600
+//      units = 200;
+
+// }
+// if (units > 100 && units <=200 ) {
+//     amount += (units - 100) * 6; //1600
+//     units = 100;
+
+// }
+// amount += units* 4//400
+// console.log(amount);
