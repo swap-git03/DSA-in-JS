f354f6ddfb8c11897db8a5df699d8d8fc23ca135
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-10-26 18:53:19 +0530

    /

--- day-4.js
+++ day-4.js
@@ -17,10 +17,10 @@
 // }
 
 // 26]
-let n = 5;
-for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= i; j++) {
-        process.stdout.write(j+ " ");
-    }
-    console.log();
-}
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         process.stdout.write(j+ " ");
+//     }
+//     console.log();
+// }
