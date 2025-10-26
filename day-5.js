c9ec3878b0ab4ade137a87269d8789b193a23e8c
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-10-26 23:38:24 +0530

    /

---
+++ day-5.js
@@ -0,0 +1,14 @@
+// 29]
+// let arr = new Array(5);
+// for (let i=0; i<arr.length;i++){
+//     arr[i] = Number(prompt("enter a num"))
+// }
+// console.log(arr);
+
+// 30]
+let arr = [10,20,30,40,50];
+let sum = 0;
+for (let i = 0; i<arr.length; i++){
+    sum = sum + arr[i];
+}
+console.log(sum);
