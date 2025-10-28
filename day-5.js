ca941cbf2e7b5d59e67fb713c5f21417635de38e
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-10-28 15:41:27 +0530

    day-5 done on 28-10-2025

--- day-5.js
+++ day-5.js
@@ -26,16 +26,46 @@
 
 
 // 32 ]
-let arr = [90, 26, 9, 72, 12];
-let max = Math.max(arr[0], arr[1]);
-let smax = Math.min(arr[0], arr[1]);
+// let arr = [90, 26, 9, 72, 12];
+// let max = Math.max(arr[0], arr[1]);
+// let smax = Math.min(arr[0], arr[1]);
 
-for (let i =2; i<arr.length; i++){
-    if (arr[i]>max){
-        smax=max;
-        max= arr[i]
-    } else if (arr[i]>smax){
-        smax=arr[i]
-    }
-}
-console.log(smax);
+// for (let i =2; i<arr.length; i++){
+//     if (arr[i]>max){
+//         smax=max;
+//         max= arr[i]
+//     } else if (arr[i]>smax){
+//         smax=arr[i]
+//     }
+// }
+// console.log(smax);
+
+
+// 33] 
+// let arr =  [10,20, 30, 40, 50];
+// let i = 0, j= arr.length- 1;
+
+// while (i != j){
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++
+//     j--
+// }
+// console.log(arr);
+
+
+// 34] 
+// let arr = [0,1,0,1,0,0,1];
+// let i = 0, j = 0;
+
+// while (i!=arr.length){
+//     if (arr[i]==0){
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     j++
+//     }
+//     i++
+// }
+// console.log(arr);
